8187c4f33a0d9fc600b9775c12566138ae3fbcd9
Yereeme <aymanyereemkone@gmail.com>
2023-02-05 16:59:47 -0500

    f

--- My project/Assets/JPAssets/Scripts/LevelManager.cs
+++ My project/Assets/JPAssets/Scripts/LevelManager.cs
@@ -1,23 +1,31 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Diagnostics;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class LevelManager : MonoBehaviour
 {
-
+   
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.CompareTag("LV1"))
         {
-            Debug.Log("Loading Level 2");
+            
             SceneManager.LoadScene("Level 2");
         }
 
         if (collision.gameObject.CompareTag("LV2"))
         {
-            Debug.Log("Loading Level 3");
+            
             SceneManager.LoadScene("Level 3");
         }
+
+        if (collision.gameObject.CompareTag("Win"))
+        {
+            
+            SceneManager.LoadScene("Win Scene");
+        }
+
     }
 }
