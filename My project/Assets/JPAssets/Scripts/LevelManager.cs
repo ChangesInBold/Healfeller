734944529eeba09f86924b2eda92d96373c3a66d
JDPX5 <Jevon.Parker@ubalt.edu>
2023-02-04 23:01:38 -0500

    Added Scene Transition
    -Started adding gameobjects to transition to next scene

--- My project/Assets/JPAssets/Scripts/LevelManager.cs
+++ My project/Assets/JPAssets/Scripts/LevelManager.cs
@@ -6,14 +6,15 @@ using UnityEngine.SceneManagement;
 public class LevelManager : MonoBehaviour
 {
 
-    private void OnCollisionEnter2D(Collider2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.gameObject.CompareTag("Player") && gameObject.CompareTag("LV1"))
+        if (collision.gameObject.CompareTag("LV1"))
         {
             Debug.Log("Loading Level 2");
             SceneManager.LoadScene("Level 2");
         }
-        if (collision.gameObject.CompareTag("Player") && gameObject.CompareTag("LV2"))
+
+        if (collision.gameObject.CompareTag("LV2"))
         {
             Debug.Log("Loading Level 3");
             SceneManager.LoadScene("Level 3");
